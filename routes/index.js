ce42ecdec08336533338483e3debb01753c8e1d8
byui-dev <okdiphala@hotmail.com>
2026-05-14 19:44:48 +0200

    Connect application to MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => { res.send('Hello World!');});
+// router.get('/', (req, res) => { res.send('Hello World!');});
+
+router.use('/contacts', require('./contacts')); // Use the contacts router for /contacts routes
 
 module.exports = router;
