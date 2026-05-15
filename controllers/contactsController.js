5cd4728ed6ba15d906b3416251cdfdc8ffa89270
byui-dev <okdiphala@hotmail.com>
2026-05-15 12:14:57 +0200

    Refactor routes/data/database.js

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -12,7 +12,7 @@ const getSingle = async (req, res) => {
     const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().collection('Contacts').find({ _id: contactId });
     const contacts = await result.toArray();
-        res.setHeader('Content-Type', 'application/json');
+        
         res.status(200).json(contacts[0]);
 };
 
