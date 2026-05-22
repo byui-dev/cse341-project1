5a2acba7a0d94591eeaf890635aff2be2fc59090
byui-dev <okdiphala@hotmail.com>
2026-05-22 22:19:09 +0200

    Add swagger route to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
-// router.get('/', (req, res) => { res.send('Hello World!');});
+router.use('/', require('./swagger')); // Use the swagger router for /swagger routes
+
+router.get('/', (req, res) => {
+    // #swagger.tags = ['Hello World'];
+    res.send('Hello World!');
+});
 
 router.use('/contacts', require('./contacts')); // Use the contacts router for /contacts routes
 
