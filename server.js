10ee52e22bb92d7d92462fca1a5e6436908013ed
byui-dev <okdiphala@hotmail.com>
2026-05-23 16:03:35 +0200

    fix: delay server startup until database is initialized

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-const express = require('express');
-require('dotenv').config();     
+const express = require("express");
+const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger_output.json");
+const db = require("./routes/data/database");
+
+require("dotenv").config();
 
 const app = express();
-const db = require('./routes/data/database');
-const bodyParser = require('body-parser');
-
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
   next();
 });
 
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 app.use("/", require("./routes"));
 
-db.initDb((err, database) => {
-     if (err) {
-          console.error('Failed to connect to the database', err);
-          process.exit(1);
-     }
-
-     database.listCollections().toArray().then(cols => {
-       console.log('Collections:', cols);
-     });     
- 
-     app.listen(PORT, () => { console.log(` Server is running on port ${PORT}`);
-  });   
+db.initDb((err) => {
+  if (err) {
+    console.error("Failed to connect to the database", err);
+    process.exit(1);
+  } else {
+    app.listen(PORT, () => {
+      console.log(` Server is running on port ${PORT}`);
+   });
+  }
 });
+
